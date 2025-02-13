58c0a5e983a73e0602ece9be2fafd2cf6cb643d9
c9borzso <c9borzso@vasvari.org>
2025-02-13 09:32:29 +0100

    CRUD Kész

--- Kreta.Desktop/ViewModels/SchoolSubjects/SubjectsManagmentViewModel.cs
+++ Kreta.Desktop/ViewModels/SchoolSubjects/SubjectsManagmentViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Kreta.Desktop.ViewModels.Base;
 using Kreta.HttpService.Services;
 using Kreta.Shared.Models.Entites;
+using Kreta.Shared.Models.Responses;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -40,6 +42,42 @@ namespace Kreta.Desktop.ViewModels.SchoolSubjects
             await base.InitializeAsync();
         }
 
+        [RelayCommand]
+        private void DoNewSubject()
+        {
+            ClearForm();
+        }
+
+        [RelayCommand]
+        private async Task DoDelete(Subject subject)
+        {
+            if (subject is not null)
+            {
+                await _httpService.DeleteAsync(subject.Id);
+                ClearForm();
+                await UpdateViewAsync();
+            }
+        }
+
+        [RelayCommand]
+        private async Task DoSave(Subject subject)
+        {
+            if (subject is not null)
+            {
+                Response response;
+                if (subject.HasId)
+                {
+                    response = await _httpService.UpdateAsync(subject);
+                }
+                else 
+                {     
+                    response= await _httpService.InsertAsync(subject);
+                }
+                ClearForm();
+                await UpdateViewAsync();
+            }
+        }
+
         //1.c Adatok bekérése backendről
         private async Task UpdateViewAsync()
         {
@@ -47,6 +85,13 @@ namespace Kreta.Desktop.ViewModels.SchoolSubjects
             List<Subject> subjects = await _httpService.GetAllAsync();
             //2.a A megérkezett adatokkal újra létrehozzuk a Subjects ObservableCollection
             Subjects = new ObservableCollection<Subject>(subjects);
+        }
+
+        //Felkészülés a dolgozatra
+        private void ClearForm()
+        {
+            SelectedSubject = new Subject();
+            OnPropertyChanged(nameof(SelectedSubject));
         }
     }
 }
