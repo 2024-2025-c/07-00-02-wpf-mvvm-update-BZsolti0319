abb74eeb415f403828711964b17a383ffb8ee997
c9borzso <c9borzso@vasvari.org>
2025-02-13 09:07:51 +0100

    A lekért adatok ebben az adatstruktúrában jelennek meg majd a view-n

--- Kreta.Desktop/ViewModels/SchoolSubjects/SubjectsManagmentViewModel.cs
+++ Kreta.Desktop/ViewModels/SchoolSubjects/SubjectsManagmentViewModel.cs
@@ -1,5 +1,10 @@
-﻿using Kreta.Desktop.ViewModels.Base;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Kreta.Desktop.ViewModels.Base;
 using Kreta.HttpService.Services;
+using Kreta.Shared.Models.Entites;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 
 namespace Kreta.Desktop.ViewModels.SchoolSubjects
@@ -8,11 +13,33 @@ namespace Kreta.Desktop.ViewModels.SchoolSubjects
     {
         private readonly ISubjectHttpService _httpService;
 
+        //2. A lekért adatok ebben az adatstruktúrában jelennek meg majd a view-n
+        [ObservableProperty]
+        private ObservableCollection<Subject> _subjects = new ObservableCollection<Subject>();
+        public SubjectsManagmentViewModel()
+        {
+
+        }
+
+        //1.b konstruktorban injektáljuk a ISubjectHttpService
+        public SubjectsManagmentViewModel(ISubjectHttpService? httpService)
+        {
+            _httpService = httpService ?? throw new ArgumentNullException(nameof(httpService));
+        }
+
         //1.feladat Tantárgyak adatainak lekérése a backendről (vizsgaremek)
         //1.a Adatok menüpont kiválasztására jelenjenek meg: InitializeAsync
-        public override Task InitializeAsync()
+        public override async Task InitializeAsync()
         {
-            return base.InitializeAsync();
+            await UpdateViewAsync();
+            await base.InitializeAsync();
+        }
+
+        //1.c Adatok bekérése backendről
+        private async Task UpdateViewAsync()
+        {
+            //1.d HttpServicen keresztül backend hívás
+            List<Subject> subjects = await _httpService.GetAllAsync();
         }
     }
 }
